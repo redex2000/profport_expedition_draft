1b2a19b6aa55ab6ecc0bfcb56eda2b65131000ab
Artur Galyamov <redex2000@gmail.com>
2018-12-11 17:57:39 +0500

    понятие ассоциация

--- app/models/expedition.rb
+++ app/models/expedition.rb
@@ -1,5 +1,6 @@
 class Expedition < ApplicationRecord
-  has_many :spaceships
+  has_many :available_spaceships, -> { where working: true }, class_name: "Spaceship"
+  has_many :stock_spaceships, -> { where working: false }, class_name: "Spaceship"
   has_one :itinerary
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_11_120416) do
+ActiveRecord::Schema.define(version: 2018_12_11_124626) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -118,6 +118,7 @@ ActiveRecord::Schema.define(version: 2018_12_11_120416) do
     t.bigint "expedition_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "working", default: true
     t.index ["expedition_id"], name: "index_spaceships_on_expedition_id"
   end
 
