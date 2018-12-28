64c05422d332dc47820e865123b6579787a9deb5
Artur Galyamov <redex2000@gmail.com>
2018-12-28 15:19:05 +0500

    аутентификация с помощью devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,7 @@
 Rails.application.routes.draw do
 
+  devise_for :users
   root to: 'galaxies#index'
-
-  resources :sessions, only: %I[new create destroy]
-  get 'sign_in', to: 'sessions#new'
-  delete 'sign_out', to: 'sessions#destroy'
 
   resources :planets
   resources :expeditions
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_28_091248) do
+ActiveRecord::Schema.define(version: 2018_12_28_100500) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -155,9 +155,13 @@ ActiveRecord::Schema.define(version: 2018_12_28_091248) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "email", null: false
-    t.string "password", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["expedition_id"], name: "index_users_on_expedition_id"
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "details", "spaceships"
