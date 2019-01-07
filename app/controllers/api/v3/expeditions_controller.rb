097a7eb629c4293dc663b20a5d600ee20a7d5885
Artur Galyamov <redex2000@gmail.com>
2019-01-07 20:05:11 +0500

    можно передавать данные закодированные

--- app/controllers/api/v3/expeditions_controller.rb
+++ app/controllers/api/v3/expeditions_controller.rb
@@ -2,5 +2,10 @@ module Api::V3
   class ExpeditionsController < ApiController
     include Api::ExpeditionsController
 
+    def show
+      @expedition = Expedition.find(params[:id])
+      render json: JsonWebToken.encode({data: JSON.parse(::V1::ExpeditionSerializer.new(@expedition).serialized_json), exp: (Time.zone.now + 7200).to_i})
+    end
+
   end
 end
