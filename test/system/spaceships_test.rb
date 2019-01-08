eb799763bd4312c12380754325d590b25131588a
Artur Galyamov <redex2000@gmail.com>
2019-01-08 13:54:24 +0500

    использование css-селекторов

--- test/system/spaceships_test.rb
+++ test/system/spaceships_test.rb
@@ -5,13 +5,13 @@ class SpaceshipsTest < ApplicationSystemTestCase
     sign_in
 
     create(:expedition)
-    create(:expedition, title: 'На Арктур')
+    expedition = create(:expedition, title: 'На Арктур')
 
     visit new_spaceship_path
 
     fill_in 'Title', with: 'Быстроход'
     fill_in 'Velocity', with: '100'
-    select 'На Арктур', from: 'Expedition'
+    find('#spaceship_expedition_id').find("option[value='#{expedition.id}']").select_option
 
     click_on 'Create'
 
