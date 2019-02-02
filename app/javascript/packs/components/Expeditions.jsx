03c813e3bf025d94175854f61adde2a179184bbe
Artur Galyamov <redex2000@gmail.com>
2019-02-02 08:32:44 +0500

    запрос к API

--- app/javascript/packs/components/Expeditions.jsx
+++ app/javascript/packs/components/Expeditions.jsx
@@ -2,28 +2,32 @@ import React from 'react';
 import Expedition from "./Expedition";
 
 export default class Expeditions extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            expeditions: []
+  constructor(props) {
+    super(props);
+    this.state = {
+      expeditions: []
+    }
+  }
+
+  componentDidMount() {
+    fetch('/v1/expeditions.json',
+      {
+        method: 'get',
+        headers: {
+          'Authorization': 'Token fyy-ifafEy7vy97M-REq'
         }
-    }
+      })
+      .then((response) => response.json())
+      .then((result) => console.log(result))
+      .catch(err => console.error('Ошибка получения данных'))
+  }
 
-    componentDidMount() {
-        fetch('/expeditions.json')
-            .then((response) => response.json())
-            .then((result) => this.setState({
-                expeditions: result
-            }))
-            .catch(err => console.error('Ошибка получения данных'))
-    }
-
-    render() {
-        const {expeditions} = this.state;
-        return expeditions.map((expedition) => {
-            return (
-                <Expedition data={expedition}/>
-            );
-        })
-    }
+  render() {
+    const {expeditions} = this.state;
+    return expeditions.map((expedition) => {
+      return (
+        <Expedition data={expedition}/>
+      );
+    })
+  }
 }
